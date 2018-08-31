a1208480c9663fd2de39c9a21fd49509257bc960
Test <test@Tests-MacBook-Pro.local>
2018-08-31 21:45:07 +0400

    Added a custom user profile model and the migrations to our databasels

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -25,7 +25,7 @@ class UserProfileManager(BaseUserManager):
     def create_superuser(self,name,email,password):
         """Creates and saves a new superuser with given details."""
 
-        user = self.create_user(email, name, password)
+        user = self.Create_User(email, name, password)
 
         user.is_superuser= True
         user.is_staff=True
